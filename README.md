1c8d566b1be979e106e88193ee8b0641f4cccb26
dkearns-lumination <dkearns@lumination.com.au>
2023-05-11 08:30:39 +0930

    Initialise health checker hosting server

---
+++ README.md
@@ -0,0 +1,3 @@
+# LeadMeLabs Health Checker Hosting Server
+
+A basic hosting server for downloading the latest version of Labs Health Checker
---
+++ server.js
@@ -0,0 +1,19 @@
+// Dependencies
+// =============================================================
+const compression = require('compression');
+const express = require('express');
+
+// Set up the Express App
+// =============================================================
+const app = express();
+const port = process.env.PORT || 8082;
+
+// Using compression npm to improve performance
+app.use(compression());
+
+// Sets up the Express app to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use('/static/health-checker', express.static('applications/health-checker'))
+
+app.listen(port, () => console.log(`Listening on port ${port}!`));
