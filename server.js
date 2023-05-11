2d2a2ca8a4d83cc4074d8cd92440468d6b9e91ac
dkearns-lumination <dkearns@lumination.com.au>
2023-05-11 10:25:40 +0930

    Enable rate limiting and disable powered by header

--- server.js
+++ server.js
@@ -8,12 +8,23 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 8082;
 
+const limiter = rateLimit({
+    windowMs: 1 * 60 * 1000, // 1 minute
+    max: 10, // maximum requests from IP address
+    standardHeaders: true,
+    legacyHeaders: false
+})
+
 // Using compression npm to improve performance
 app.use(compression());
 
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(limiter)
+
+app.disable('x-powered-by')
+
 app.use('/static/health-checker', express.static('applications/health-checker'))
 
 app.listen(port, () => console.log(`Listening on port ${port}!`));
